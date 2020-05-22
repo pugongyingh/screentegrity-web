47df332c2e251f1bb3dbd253f6038ea2301b559b
pugongyingh <pgyhh@sina.cn>
2020-05-22 17:34:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const path = require('path')
-const jwt = require('jsonwebtoken')
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
-const User = require('./models/User')
 
-const app = express()
-
-
-
-
-
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(8080, function () {
-  console.log('app is listening at port 8080');
+app.listen(80, function () {
+  console.log('app is listening at port 80');
+});
